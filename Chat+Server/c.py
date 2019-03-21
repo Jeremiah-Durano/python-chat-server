9fc6e677687d4649e7e792d18f3fe4b1a2fd982b
Sukhdev <b00100206@student.itb.ie>
2019-03-21 15:34:15 +0000

    Ians work

--- Chat+Server/c.py
+++ Chat+Server/c.py
@@ -2,12 +2,12 @@
 # Echo client program
 import socket
 
-HOST = '127.0.0.1'    # The remote host
-PORT = 50007          # The same port as used by the server
+HOST = '127'    # The remote host
+PORT = 20017  # The same port as used by the server
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((HOST, PORT))
-data = s.recv(80000)
+#data = s.recv(80000)
 
 print "please enter your username"
 name = raw_input()
--- Chat+Server/s.py
+++ Chat+Server/s.py
@@ -4,7 +4,7 @@ import threading, Queue
 from time import gmtime, strftime
 import time
 import hashlib
-import webbrowser
+
 
 HOST = '127.0.0.1'
 PORT = 50007
@@ -18,16 +18,6 @@ s.bind((HOST, PORT))
 # Each message in the buffer is separated by a colon :
 buffer = " "
 connections = []
-
-#Google Function
-def google(data):
-    print "Google function"
-    googleStr = data.replace("!google","")
-    googleStr = googleStr.split(':', 1)[-1]
-    googleStr = googleStr.strip()
-    googleStr = googleStr.replace(" ","+")
-    url = "https://www.google.com/search?q=" + googleStr
-    webbrowser.open_new_tab(url)
 
 # custom say hello command
 def sayHello():
@@ -53,9 +43,6 @@ def parseInput(data, con):
 	
 	start_time = time.time()
 	print("--- %s seconds ---" % (time.time() - start_time))
-
-  if "!google" in data:
-    google(data)
 
 # we a new thread is started from an incoming connection
 # the manageConnection funnction is used to take the input
